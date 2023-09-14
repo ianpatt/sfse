bfb788e307a9cddbfe1ee1dcda043cdbdf308123
ianpatt <ianpatt@gmail.com>
2023-09-14 15:20:54 -0700

    0.1.2

--- sfse_common/sfse_version.h
+++ sfse_common/sfse_version.h
@@ -4,10 +4,10 @@
 // these have to be macros so they can be used in the .rc
 #define SFSE_VERSION_INTEGER		0
 #define SFSE_VERSION_INTEGER_MINOR	1
-#define SFSE_VERSION_INTEGER_BETA	1
-#define SFSE_VERSION_VERSTRING		"0, 0, 1, 1"
+#define SFSE_VERSION_INTEGER_BETA	2
+#define SFSE_VERSION_VERSTRING		"0, 0, 1, 2"
 #define SFSE_VERSION_PADDEDSTRING	"0001"
-#define SFSE_VERSION_RELEASEIDX		2
+#define SFSE_VERSION_RELEASEIDX		3
 
 #define MAKE_EXE_VERSION_EX(major, minor, build, sub)	((((major) & 0xFF) << 24) | (((minor) & 0xFF) << 16) | (((build) & 0xFFF) << 4) | ((sub) & 0xF))
 #define MAKE_EXE_VERSION(major, minor, build)			MAKE_EXE_VERSION_EX(major, minor, build, 0)
@@ -34,7 +34,7 @@
 // information about the state of the game at the time of release
 #define SFSE_TARGETING_BETA_VERSION	0
 #define CURRENT_RELEASE_RUNTIME		RUNTIME_VERSION_1_7_29
-#define CURRENT_RELEASE_SFSE_STR	"0.1.1"
+#define CURRENT_RELEASE_SFSE_STR	"0.1.2"
 
 #if GET_EXE_VERSION_SUB(RUNTIME_VERSION) == RUNTIME_TYPE_BETHESDA
 #define SAVE_FOLDER_NAME "Starfield"
